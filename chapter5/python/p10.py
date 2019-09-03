889bd30c7b766517b76f456c5b5a19cd4b33b3d0
pinakm9 <pinakm9@gmail.com>
2019-09-03 10:46:45 +0530

    p10 theory described in problems.pdf

--- chapter5/python/p10.py
+++ chapter5/python/p10.py
@@ -1,21 +1,10 @@
 # This script requires 1 command line argument: sample size
 import sys
-import numpy as np
-import scipy.stats as ss
-from simulate import Simulation
+from simulate import InverseComposition, RVContinuous
 
-# Implements gamma distribution
-class Gamma(Simulation):
-    def __init__(self, n, l):
-        Simulation.__init__(self)
-        self.n = n # parameter n for gamma distribution
-        self.l = float(l) # parameter lambda for gamma distribution
-        self.method = lambda: -np.log(np.prod(np.random.uniform(0.0, 1.0, self.n)))/self.l
+# unpack command-line arguments
+sample_size = int(sys.argv[1])
 
-gamma = Gamma(1000, 1/800.0)
-gamma.generate(int(sys.argv[1]))
-target_mean = gamma.n/gamma.l
-target_var = gamma.n/gamma.l**2
-target_dist = ss.gamma(1000, scale = 1/800.0).cdf
-gamma.vis_cdf_man(target_dist, [0.0, 10.0], target_mean, target_var, file_path = '../images/p10_dist_{}_{}.png'.format('incomp', sys.argv[1]), display = True)
-print("Simulated probabilty = {}\nTarget probabilty = {}".format(1 - gamma.ecdf(1e6), 1 - target_dist(1e6)))
+# target probability distribution
+def target_cdf(x, *args):
+    return (x + x**3 + x**5)/3.0
